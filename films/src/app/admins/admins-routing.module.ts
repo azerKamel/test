500c8ebe58947fbcdfc97c960d3df1d35ab67fd6
azerKamel <guidekamel@gmail.com>
2026-03-25 19:22:57 +0000

    first

--- films/src/app/admins/admins-routing.module.ts
+++ films/src/app/admins/admins-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminsComponent } from './admins.component';
+import { ListerFilmsComponent } from './lister-films/lister-films.component';
+import { AjouterFilmComponent } from './ajouter-film/ajouter-film.component';
 
-const routes: Routes = [{ path: '', component: AdminsComponent }];
+const routes: Routes = [{ path: '', component: AdminsComponent ,children: [
+{path: 'liste-films', component: ListerFilmsComponent },
+{path: 'add-film', component: AjouterFilmComponent},
+] ,}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
