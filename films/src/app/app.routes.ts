01b4aef29a439156634bcd74125c4c0c4cf113f9
azerKamel <guidekamel@gmail.com>
2026-03-25 18:36:44 +0000

    teste

--- films/src/app/app.routes.ts
+++ films/src/app/app.routes.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'visiteur', loadChildren: () => import('./visiteur/visiteur.module').then(m => m.VisiteurModule) },
+    { path: 'admins', loadChildren: () => import('./admins/admins.module').then(m => m.AdminsModule) }
+];
 @NgModule({
 imports: [RouterModule.forRoot(routes,
 {preloadingStrategy: PreloadAllModules}
 )],
 exports: [RouterModule]
 })
-export  class  AppRoutingModule{}
+export  class  AppRoutingModule{
+
+
+
+
+}
