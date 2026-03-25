4f383ef765de42900e79212ea0ad20bbe27c0e88
azerKamel <guidekamel@gmail.com>
2026-03-25 19:03:32 +0000

    routting

--- films/src/app/visiteur/visiteur-routing.module.ts
+++ films/src/app/visiteur/visiteur-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { VisiteurComponent } from './visiteur.component';
+import { AproposComponent } from './apropos/apropos.component';
+import { HomeVisiteurComponent } from './home-visiteur/home-visiteur.component';
 
-const routes: Routes = [{ path: '', component: VisiteurComponent }];
+const routes: Routes = [{ path: '', component: VisiteurComponent,
+children:[
+  {path: 'apropos', component: AproposComponent },
+{path: 'home-visiteur', component: HomeVisiteurComponent },
+]
+
+ }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
